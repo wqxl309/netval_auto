800950496357fdc241ed65f24e4a11291a5b9521
Jiapeng <wqxl309@hotmail.com>
2017-03-14 13:58:09 +0800

    elements extract done

--- src/base_class.py
+++ src/base_class.py
@@ -1,18 +1,24 @@
 import sqlite3
 import xlrd
 import os
-import time
 
 __metaclass__ = type
 
 class db:
 
     def __init__(self,dbdir,filedir,flistdir,updtlstdir,netvaldir):
+        if not os.path.exists(flistdir):
+            os.system('type null > '+filedir)
+
+        if not os.path.exists(updtlstdir):
+            os.system('type null > '+updtlstdir)
+
         self.dbdir=dbdir
         self.filedir=filedir
         self.flistdir=flistdir
         self.updtlstdir=updtlstdir
         self.netvaldir=netvaldir
+
 
     def dbexist(self):
         # 若db不存在则不能写入table,db创建在类外实现
@@ -43,8 +49,9 @@ class db:
             print('Database does NOT exist, no update!')
             return False
         hastb=self.tbexist(tbdir)
+        tbname=self.get_tablename(tbdir)
         if hastb:
-            print('Table already exists, no update!')
+            print(tbname+' already exists, no update!')
             return False
         # 寻找起始行
         data=xlrd.open_workbook(tbdir)
@@ -69,7 +76,14 @@ class db:
         tablename=self.get_tablename(tbdir)
         titles=self.get_tbtitles(tbdir)
         titlenum=len(titles)
-        titletrans= ('%s,'*titlenum)[0:(3*titlenum-1)] % tuple(titles)
+        titlestr=[]
+        for tl in titles:
+            if tl in ('科目代码', '科目名称', '币种','科目级别'):
+                titlestr.append(tl+' TEXT,')
+            else:
+                titlestr.append(tl+' REAL,')
+        titletrans=''.join(titlestr)[:-1]
+        #titletrans= ('%s,'*titlenum)[0:(3*titlenum-1)] % tuple(titles)
         exeline=''.join(['CREATE TABLE ',tablename,' (',titletrans,') '])
         c.execute(exeline)
         print('Table '.join([tablename,' created!']))
@@ -91,7 +105,6 @@ class db:
 
     def update_tables(self):
         # 检查是否有新文件，如有则更新,并把更新后的文件写入到 flistdir 中
-        # 检测是否存在 flistdir 是否存在，不存在则建立一个
         # 检查新增加的文件
         filelist_w=open(self.flistdir,'a+')
         filelist_r=open(self.flistdir)
@@ -118,7 +131,7 @@ class db:
             filelist_w.close()
 
 
-    def get_updtlst(self):
+    def get_updtlst(self):   # 提取需要更新的表
         if not self.dbexist():
             print('Data db does NOT exist!')
             return
@@ -130,7 +143,6 @@ class db:
             raise
         finally:
             conn_db.close()
-
         netfile=open(self.updtlstdir)
         try:
             nettbs=netfile.readlines()
@@ -138,7 +150,6 @@ class db:
             raise
         finally:
             netfile.close()
-
         dbtables=[]
         [dbtables.append(val[0]) for val in dbtbs]
         nettables=[]
@@ -149,8 +160,11 @@ class db:
 
     def update_netval(self):
         updtlst=self.get_updtlst()
+        if len(updtlst)==0:
+            print('No new tables to update !')
         conn_netval=sqlite3.connect(self.netvaldir)
         conn_db=sqlite3.connect(self.dbdir)
+        conn_db.row_factory=sqlite3.Row
         cn=conn_netval.cursor()
         cd=conn_db.cursor()
         netfile=open(self.updtlstdir,'a+')
@@ -158,14 +172,66 @@ class db:
             nettbs=cn.execute(' SELECT name FROM sqlite_master WHERE type=\'table\' ').fetchall()
             if not ('Net_Values',) in nettbs:
                 print('Net_Values table created')
-                cn.execute('CREATE TABLE Net_Values (share,asstot,liatot,nettot)')
-                for dumi in range(len(updtlst)):
-                    tb=updtlst[dumi]
-                    exeline='SELECT 科目代码,市值本币 FROM '+ tb +' WHERE 科目代码 IN (\'实收资本\',\'资产合计\',\'负债合计\',\'资产净值\')'
-                    rawstaff=cd.execute(exeline).fetchall()
-                    cn.execute("INSERT INTO Net_Values VALUES (?,?,?,?)" , (rawstaff[0][-1], rawstaff[1][-1],rawstaff[2][-1],rawstaff[3][-1]) )
-                    conn_netval.commit()
-                    netfile.write(tb+'\n')
+                cn.execute('CREATE TABLE Net_Values (date,sharenum,assettot,debttot,assetnet,servfee,keepfee,mangfee,earn,buy,sell)')
+
+            for dumi in range(len(updtlst)):
+                tb=updtlst[dumi]
+                cols=cd.execute('SELECT * FROM '+tb).fetchone().keys()
+                if '市值本币' in cols:
+                    valcol='市值本币'
+                else:
+                    valcol='市值'
+
+                print(tb)
+
+                exeline1=''.join(['SELECT ',valcol,' FROM ',tb ,' WHERE 科目代码 IN (\'实收资本\',\'资产合计\',\'负债合计\',\'资产净值\')'])
+                netinfo=cd.execute(exeline1).fetchall()
+                sharenum=netinfo[0][0]
+                assettot=netinfo[1][0]
+                debttot =netinfo[2][0]
+                assetnet=netinfo[3][0]
+
+                exeline2=''.join(['SELECT ',valcol,' FROM ',tb ,' WHERE 科目代码 IN (\'2206.14\',\'2207.01\')'])  #行政服务费，托管费
+                othinfo=cd.execute(exeline2).fetchall()
+                servfee=othinfo[0][0]
+                keepfee=othinfo[1][0]
+
+                exeline3=''.join(['SELECT ',valcol,' FROM ',tb ,' WHERE 科目代码 IN (\'2206.01\')'])  # 管理费, 没有则为0
+                mfeinfo=cd.execute(exeline3).fetchall()
+                if len(mfeinfo)==0:
+                    mangfee=0
+                else:
+                    mangfee=mfeinfo[0][0]
+
+                exeline4=''.join(['SELECT ',valcol,' FROM ',tb ,' WHERE 科目代码 IN (\'2206.02\')']) # 业绩报酬， 没有则为0
+                earninfo=cd.execute(exeline4).fetchall()
+                if len(earninfo)==0:
+                    earn=0
+                else:
+                    earn=earninfo[0][0]
+
+                exeline5=''.join(['SELECT ',valcol,' FROM ',tb ,' WHERE 科目代码 IN (\'1207.01\')']) # 应收申购款， 没有则为0
+                buyinfo=cd.execute(exeline5).fetchall()
+                if len(buyinfo)==0:
+                    buy=0
+                else:
+                    buy=buyinfo[0][0]
+
+                exeline6=''.join(['SELECT ',valcol,' FROM ',tb ,' WHERE 科目代码 IN (\'2203.01\')']) # 应收赎回款， 没有则为0
+                sellinfo=cd.execute(exeline6).fetchall()
+                if len(sellinfo)==0:
+                    sell=0
+                else:
+                    sell=sellinfo[0][0]
+
+                cn.execute("INSERT INTO Net_Values VALUES (?,?,?,?,?,?,?,?,?,?,?)" , (tb[-8:],sharenum,assettot,debttot,assetnet,servfee,keepfee,mangfee,earn,buy,sell) )
+                conn_netval.commit()
+
+                print(tb[-8:],sharenum,assettot,debttot,assetnet,servfee,keepfee,mangfee,earn,buy,sell)
+                
+                netfile.write(tb+'\n')
+                print( tb+' update finished ! ' )
+
         except:
             raise
         finally:
